0c58c4fbc8e932c2e9331447fc70d75bca726d5e
susmitamenka <susmitaprasad73@gmail.com>
2025-02-13 22:19:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const student={
+	name:"aman",
+	age:28,
+	city:"Mumbai"
+}
+function getKeys(obj) {
+	return Object.keys(obj);
+	}
+console.log(getKeys(student));
